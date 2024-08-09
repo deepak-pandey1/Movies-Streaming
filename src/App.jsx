9eaa3e7901173cfd2b2420904a9befa9411933cf
Deepak Pandey <deepakpandeyofficially@gmail.com>
2024-08-09 18:37:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
                 <Route index element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
-                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+                <Route path="/*" element={<Home />}></Route>
             </Routes>
         </Router>
     </div>
